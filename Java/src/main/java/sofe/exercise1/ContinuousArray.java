7c650f169e931d759a84dccee685f5468510515a
Martin <jomaburciaga@hotmail.com>
2017-10-07 20:51:26 -0600

    Martin BUrciaga , Javier Venegas -equipo 5

--- Java/src/main/java/sofe/exercise1/ContinuousArray.java
+++ Java/src/main/java/sofe/exercise1/ContinuousArray.java
@@ -3,7 +3,26 @@ package sofe.exercise1;
 public class ContinuousArray {
 
     public static boolean isAlmostIncreasingSequence(int[] sequence) {
-        return null;
+        boolean bandera = true;
+        boolean bandera2 = false;
+        for (int i = 0; i < sequence.length - 1; i++) {
+
+            if (sequence[i] < sequence[i + 1]) {
+                bandera = true;
+
+            } else {
+                if (bandera2) {
+                    bandera = false;
+                    break;
+                } else {
+                    bandera = false;
+                    bandera2=true;
+                }
+            }
+        }
+
+        return bandera;
+
     }
 
 }
--- Java/src/main/java/sofe/exercise2/ReverseParenthesis.java
+++ Java/src/main/java/sofe/exercise2/ReverseParenthesis.java
@@ -1,9 +1,80 @@
 package sofe.exercise2;
 
+import java.util.ArrayList;
+
 public class ReverseParenthesis {
 
     public static String reverseParenthesis(String text) {
-        return "";
+        String cadena = "";
+        int counter = 0;
+        ArrayList abiertos = posicionParentesisAbiertos(text);
+        ArrayList cerrados = posicionParentesisCerrados(text);
+        String inicio=text.substring(0,Integer.parseInt(abiertos.get(0).toString()));
+        String finals=text.substring(Integer.parseInt(cerrados.get(0).toString()));
+     //   String[] parejas = new String[text.length()];
+        ArrayList Parejas=new ArrayList();
+        for (int i = 0; i < abiertos.size(); i++) {
+            int auxPosicionFinal=0;
+            
+            int auxPosicionInicial=Integer.parseInt(abiertos.get(i).toString());
+            for(int j= 0; j<cerrados.size(); j++)
+            {
+                if(auxPosicionInicial >Integer.parseInt(cerrados.get(j).toString()))
+                {
+                    
+                }
+                else{
+                    auxPosicionFinal=Integer.parseInt(cerrados.get(j).toString());
+                    break;
+                }
+            }
+            
+            
+           
+            StringBuilder aux = new StringBuilder(text.substring(auxPosicionInicial+1,auxPosicionFinal-1));
+
+            Parejas.add(aux.reverse().toString());
+            /*for (int j = abiertos[i]; j < cerrados[i]; j++) {
+
+                char aux2 = parejas[counter].charAt(j);
+                char aux3 = text.charAt(j);
+                aux3 = aux2;
+
+            }*/
+           
+        }
+        cadena=inicio+Parejas.get(Parejas.size()-1).toString()+finals; 
+       
+
+        return cadena;
+    }
+       
+    static ArrayList posicionParentesisAbiertos(String text) {
+        int counter = 0;
+        ArrayList lista=new ArrayList();
+        int[] arreglo = new int[text.length()];
+        for (int i = text.length() - 1; i >= 0; i--) {
+            if (text.charAt(i) == '(') {
+                lista.add(i);
+
+            }
+        }
+            
+        return lista;
+
+    }
+
+    static ArrayList posicionParentesisCerrados(String text) {
+        ArrayList lista=new ArrayList();
+        int counter = 0;
+        for (int i = 0; i < text.length(); i++) {
+            if (text.charAt(i) == ')') {
+                lista.add(i);
+
+            }
+        }
+
+        return lista;
     }
 
 }
